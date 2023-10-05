962e50ef3923b27d85914335c30fcd47797b115c
Andebugan <andreibugaenko2012@mail.ru>
2023-10-05 19:24:40 +0300

    working on command parser tests

--- SimpleCommandParser/TestSimpleCommandParser/TestParser/TestCommandParser.cs
+++ SimpleCommandParser/TestSimpleCommandParser/TestParser/TestCommandParser.cs
@@ -5,9 +5,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Moq;
+using SimpleCommandParser.Option;
 
 namespace TestSimpleCommandParser.TestParser {
     public class TestCommandParser {
-        
+
+        public static IEnumerable<object[]> TestCommandParserData() {
+            IOption option_1 = Mock.Of<IOption>();
+
+            ICommandOptionParser commandOptionParser1 = Mock.Of<ICommandOptionParser>();
+
+            ICommandParser commandParser = Mock.Of<ICommandParser>();
+            commandParser.Name = "test";
+            commandParser.Options = new List<ICommandOptionParser> {
+
+            };
+
+            yield return new object[] { };
+            yield return new object[] { };
+            yield return new object[] { };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestCommandParserData)]
+        public void TestCommandNormalOptionParsing() {
+            
+        }
+
+        [Fact] void TestCommandIncorrectOptionParsing() {
+
+        }
     }
 }
